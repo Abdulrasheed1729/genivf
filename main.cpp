0390e541fcfce54afc182e6888f24f8c57e38bf9
Abdulrasheed Fawole <fawomath@gmail.com>
2026-06-29 15:49:26 +0200

    remove commented code in `main.cpp`

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
+// TODO: remove this file in future
 int
 main()
 {
-    // std::ofstream tsv_file("query_results.tsv");
-    // tsv_file << "query_id\tquery_header\ttarget_sequence\ttarget_start_"
-    //             "pos\tdistance\n";
 }
